cdd1c994a8de891686a78ea999d64dcf9f503cca
sh4dov <arojek.work@gmail.com>
2015-03-18 18:52:57 +0100

    tests

---
+++ RandomInteligence/CommandRandomizer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cerberus;
+using RandomInteligence.Commands;
+
+namespace RandomInteligence
+{
+    public class CommandRandomizer
+    {
+        private readonly List<CommandInterval> commands;
+        private readonly int maxInterval;
+        private readonly Random random;
+
+        public CommandRandomizer(Command[] commands, int? seed = null)
+        {
+            Guard.CollectionIsNotNullOrEmpty(() => commands);
+            var interval = 0;
+            this.commands = commands.Select(c =>
+            {
+                var start = interval;
+                var end = interval + c.Priority - 1;
+                interval = end + 1;
+                return new CommandInterval(c, start, end);
+            }).ToList();
+            maxInterval = interval;
+            random = seed.HasValue ? new Random(seed.Value) : new Random();
+        }
+
+        public Command GetRandom()
+        {
+            var interval = random.Next() % maxInterval;
+            return commands.First(c => c.IsInInterval(interval)).Command;
+        }
+
+        private class CommandInterval
+        {
+            private readonly int end;
+            private readonly int start;
+
+            public CommandInterval(Command command, int start, int end)
+            {
+                this.start = start;
+                this.end = end;
+                Command = command;
+            }
+
+            public Command Command { get; set; }
+
+            public bool IsInInterval(int value)
+            {
+                return start >= value && value <= end;
+            }
+        }
+    }
+}
---
+++ Sandbox/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using RandomInteligence;
+using RandomInteligence.Commands;
+
+namespace Sandbox
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var world = new[,]
+            {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+            };
+
+            while (true)
+            {
+                var commandRandomizer = new CommandRandomizer(new Command[] { new CommandLeft(world), new CommandRight(world), new CommandUp(world), new CommandDown(world) });
+
+                CommandMove cmd;
+                long steps = 0;
+                do
+                {
+                    cmd = (CommandMove)commandRandomizer.GetRandom();
+                    cmd.Move();
+                    steps++;
+                } while (!cmd.Found());
+                Console.WriteLine("Found way in {0} steps", steps);
+            }
+        }
+    }
+
+    internal abstract class CommandMove : Command
+    {
+        private readonly int[,] world;
+        protected int x, y;
+        protected int maxX;
+        protected int maxY;
+
+        public CommandMove(int[,] world)
+        {
+            this.world = world;
+            maxX = world.GetLength(0) - 1;
+            maxY = world.GetLength(1) - 1;
+        }
+
+        public abstract void Move();
+
+        public bool Found()
+        {
+            return world[x, y] == 1;
+        }
+    }
+
+    internal class CommandLeft : CommandMove
+    {
+        public CommandLeft(int[,] world)
+            : base(world)
+        {
+        }
+
+        public override void Move()
+        {
+            if (x > 1)
+            {
+                x--;
+            }
+        }
+    }
+
+    internal class CommandRight : CommandMove
+    {
+        public CommandRight(int[,] world)
+            : base(world)
+        {
+        }
+
+        public override void Move()
+        {
+            if (x < maxX)
+            {
+                x++;
+            }
+        }
+    }
+
+    internal class CommandUp : CommandMove
+    {
+        public CommandUp(int[,] world)
+            : base(world)
+        {
+        }
+
+        public override void Move()
+        {
+            if (y > 1)
+            {
+                y--;
+            }
+        }
+    }
+
+    internal class CommandDown : CommandMove
+    {
+        public CommandDown(int[,] world)
+            : base(world)
+        {
+        }
+
+        public override void Move()
+        {
+            if (y < maxY)
+            {
+                y++;
+            }
+        }
+    }
+}
